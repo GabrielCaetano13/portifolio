d69511aac5b8b8ad6d93b0322062e2950ef0b494
GabrielCaetano13 <gabriel011depaulac@gmail.com>
2023-04-06 12:23:23 -0300

    add script para o menu responsivo

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ function animationScroll () {
 if (sectionElements.length) {
     window.addEventListener('scroll', animationScroll)
 }
+
+
+document.querySelector('.mobile-menu-icon').addEventListener('click', () => {
+    let menuMobile = document.querySelector(".mobile-menu")
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = './assets/images/open-icon.svg'
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = './assets/images/close-icon.svg'
+    }
+
+    document.querySelector('.open').addEventListener('click', () => {
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = './assets/images/open-icon.svg'
+    })
+})
+
+
+
+
